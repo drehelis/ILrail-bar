99b02890cc3f3f2119d3b5f18fcfb632a47e49e1
Danny Rehelis <danny@savvy.security>
2025-04-19 08:37:01 +0300

    docs: typo

--- utils/package_dmg.sh
+++ utils/package_dmg.sh
@@ -21,7 +21,7 @@ Since this app isn't signed with an Apple Developer certificate, macOS may show 
 To open the app:
 1. Copy the ILrail-bar app to your Applications folder
 2. Click on the app icon
-3. Greet the security warning "Apple couldn not verify 'ILrail-bar' is free from malware..."
+3. Greet with the security warning "Apple couldn not verify 'ILrail-bar' is free from malware..."
 4. Go to System Settings > Privacy & Security
 5. Scroll down to the "Security" section
 6. Look for a message about "ILrail-bar was blocked". Click "Allow Anyway"
